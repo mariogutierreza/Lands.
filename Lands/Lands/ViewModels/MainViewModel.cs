4b49c8f12e45ff23c8ef6e063ab261ba71971670
mariogutierreza <36140834+mariogutierreza@users.noreply.github.com>
2018-02-19 23:34:15 -0500

    Login View Model

--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -2,5 +2,19 @@
 {
     public class MainViewModel
     {
+        #region ViewModels
+        private LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
